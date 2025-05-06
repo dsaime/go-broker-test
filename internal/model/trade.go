9013ce406cb6dd1c9fb16c70335c494a311e769a
Saime <acc.saime.d@gmail.com>
2025-05-06 14:43:48 +0300

    написать тест валидации trade

---
+++ internal/model/trade.go
@@ -0,0 +1,55 @@
+package model
+
+import (
+	"errors"
+	"regexp"
+)
+
+type Trade struct {
+	Account string  // must not be empty
+	Symbol  string  // ^[A-Z]{6}$ (e.g. EURUSD)
+	Volume  float64 // must be > 0
+	Open    float64 // must be > 0
+	Close   float64 // must be > 0
+	Side    string  //	either "buy" or "sell"
+}
+
+const (
+	TradeSideByy  = "buy"
+	TradeSideSell = "sell"
+)
+
+var (
+	ErrInvalidAccount = errors.New("некорректное значение Account")
+	ErrInvalidSymbol  = errors.New("некорректное значение Symbol")
+	ErrInvalidVolume  = errors.New("некорректное значение Volume")
+	ErrInvalidOpen    = errors.New("некорректное значение Open")
+	ErrInvalidClose   = errors.New("некорректное значение Close")
+	ErrInvalidSide    = errors.New("некорректное значение Side")
+)
+
+var TradeSymbolRegex = regexp.MustCompile(`^[A-Z]{6}$`)
+
+func (t Trade) Validate() error {
+	if t.Account == "" {
+		return ErrInvalidAccount
+	}
+
+	if !TradeSymbolRegex.MatchString(t.Symbol) {
+		return ErrInvalidSymbol
+	}
+	if t.Volume <= 0 {
+		return ErrInvalidVolume
+	}
+	if t.Open <= 0 {
+		return ErrInvalidOpen
+	}
+	if t.Close <= 0 {
+		return ErrInvalidClose
+	}
+	if t.Side != TradeSideByy && t.Side != TradeSideSell {
+		return ErrInvalidSide
+	}
+
+	return nil
+}
---
+++ internal/model/trade_test.go
@@ -0,0 +1,136 @@
+package model
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"testing"
+)
+
+func newValidTrade() Trade {
+	return Trade{
+		Account: "732cbdf0",
+		Symbol:  "AAABBB",
+		Volume:  300000.0,
+		Open:    200000.0,
+		Close:   100000.0,
+		Side:    TradeSideByy,
+	}
+}
+
+func TestTrade_Validate(t *testing.T) {
+	t.Run("Account не должно быть пустым", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Account = ""
+		err := trade.Validate()
+		assert.ErrorIs(t, err, ErrInvalidAccount)
+	})
+	t.Run("С валидным значение Account ошибка не вернется", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Account = "asdfghjkl"
+		err := trade.Validate()
+		assert.NoError(t, err)
+	})
+
+	// Symbol
+	t.Run("Symbol должно состоять из 6ти символов", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Symbol = "1234567"
+		err := trade.Validate()
+		assert.ErrorIs(t, err, ErrInvalidSymbol)
+	})
+	t.Run("Symbol должно состоять только из букв верхнего регистра", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Symbol = "aaa333"
+		err := trade.Validate()
+		assert.ErrorIs(t, err, ErrInvalidSymbol)
+	})
+	t.Run("Symbol должно содержать только A-Z", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Symbol = "a ф1@/"
+		err := trade.Validate()
+		assert.ErrorIs(t, err, ErrInvalidSymbol)
+	})
+	t.Run("С валидным значение Symbol ошибка не вернется", func(t *testing.T) {
+		trade := newValidTrade()
+		validSymbols := []string{
+			"ABCDEF",
+			"GHIJKL",
+			"MNOPQR",
+			"STUVWX",
+			"YYYZZZ",
+		}
+		for _, symbol := range validSymbols {
+			trade.Symbol = symbol
+			err := trade.Validate()
+			require.NoError(t, err)
+		}
+	})
+
+	// Volume
+	t.Run("Volume не может быть меньше 1", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Volume = -1
+		err := trade.Validate()
+		assert.ErrorIs(t, err, ErrInvalidVolume)
+	})
+	t.Run("С валидным значение Volume ошибка не вернется", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Volume = 31289
+		err := trade.Validate()
+		assert.NoError(t, err)
+	})
+
+	// Open
+	t.Run("Open не может быть меньше 1", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Open = -1
+		err := trade.Validate()
+		assert.ErrorIs(t, err, ErrInvalidOpen)
+	})
+	t.Run("С валидным значение Open ошибка не вернется", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Open = 31289
+		err := trade.Validate()
+		assert.NoError(t, err)
+	})
+
+	// Close
+	t.Run("Close не может быть меньше 1", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Close = -1
+		err := trade.Validate()
+		assert.ErrorIs(t, err, ErrInvalidClose)
+	})
+	t.Run("С валидным значение Close ошибка не вернется", func(t *testing.T) {
+		trade := newValidTrade()
+		trade.Close = 31289
+		err := trade.Validate()
+		assert.NoError(t, err)
+	})
+
+	// Side
+	t.Run("Side может быть равным только определенным значениям", func(t *testing.T) {
+		trade := newValidTrade()
+		invalidValues := []string{
+			"",
+			"1",
+			"a",
+			"BUY",
+			"SELL",
+		}
+		for _, side := range invalidValues {
+			trade.Side = side
+			require.ErrorIs(t, trade.Validate(), ErrInvalidSide)
+		}
+
+		validValues := []string{
+			"buy",
+			"sell",
+		}
+		for _, side := range validValues {
+			trade.Side = side
+			require.NoError(t, trade.Validate())
+		}
+	})
+}
